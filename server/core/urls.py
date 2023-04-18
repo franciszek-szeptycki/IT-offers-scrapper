193612b541bd967cd990400ea8190eab9bf1c815
franciszek-szeptycki <franciszek.szepctyki@gmail.com>
2023-04-18 20:16:12 +0200

    requirements.txt

--- server/core/urls.py
+++ server/core/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', views.index, name="index"),
 ]
 
 if settings.DEBUG:
