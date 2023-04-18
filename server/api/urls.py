4863b228c2d6d3709aad94e88dcc1164d8c06071
franciszek-szeptycki <franciszek.szepctyki@gmail.com>
2023-04-18 21:48:48 +0200

    commit

--- server/api/urls.py
+++ server/api/urls.py
@@ -4,7 +4,7 @@ from api.views import info, just_join, no_fluff_jobs
 
 urlpatterns = [
     path('info/', info),
-    path('just-join/', just_join),
-    path('no-fluff-jobs/', no_fluff_jobs),
+    path('just-join', just_join),
+    path('no-fluff-jobs', no_fluff_jobs),
 ]
 
