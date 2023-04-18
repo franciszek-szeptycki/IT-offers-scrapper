8865c92ac1546adc25c6fad7a19291e88f6cc484
franciszek-szeptycki <franciszek.szepctyki@gmail.com>
2023-04-18 22:45:55 +0200

    commit

--- server/core/settings.py
+++ server/core/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'assets/'
+STATIC_URL = '/assets/'
 MEDIA_URL = '/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
